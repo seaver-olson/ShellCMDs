281754c2f916b9ab7929fc2202815c1f48b53f64
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2024-08-08 16:48:58 -0500

    Create .bash_cmds.sh

---
+++ .bash_cmds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function open(){
+        explorer.exe $1
+}
+function chrome(){
+        start 'chrome.exe'
+}
+function lookup(){
+        if [ "$1" = "-d" ]; then
+                start 'chrome.exe' "$2"
+        else
+                start 'chrome.exe' "https://google.com/search?q=$*"
+        fi
+}
+function lazyGit(){     git add .
+        git commit -m "$*"
+        git push
+}
